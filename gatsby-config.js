13b30c1304187d809e7364145ef176b531162d04
burnpiro <kemalpiro@gmail.com>
2019-07-13 16:17:19 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,50 @@
  */
 
 module.exports = {
-  /* Your site config here */
-}
+  siteMetadata: {
+    title: `QuarkOwl LTD - JavaScript Engineering Solutions`,
+    description: ``,
+    author: `QuarkOwl LTD`,
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`300`, `400`, `500`, `700`],
+          },
+          {
+            family: `Material Icons`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `QuarkOwl LTD - JavaScript Engineering Solutions`,
+        short_name: `QuarkOwl`,
+        start_url: `/`,
+        background_color: `#f7fafd`,
+        theme_color: `#00b9f1`,
+        display: `standalone`,
+        lang: `en`,
+        icon: `static/QOSquareLogo.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
+};
