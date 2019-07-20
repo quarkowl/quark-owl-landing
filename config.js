10a3cfd0caa9345dfcfae387d336191cb43efad6
burnpiro <kemalpiro@gmail.com>
2019-07-20 20:52:47 +0200

    Add missing config data, form link, and copyright sentence

--- config.js
+++ config.js
@@ -4,29 +4,36 @@ module.exports = {
   title: `QuarkOwl LTD - JavaScript Engineering Solutions`,
   short_name: `QuarkOwl LTD`,
   subtitle: 'Even complex things could be explain in a simple way.',
-  copyright: '© All rights reserved.',
+  copyright: '© QuarkOwl 2019. All rights reserved.',
   disqusShortname: '',
   contactFormUrl:
-    'https://docs.google.com/forms/d/e/1FAIpQLSdksRjFLVeWW2-wq8QSeKyJwUOs5LAq1bxsv0D9bTNEAQiu1g/formResponse',
+    'https://docs.google.com/forms/d/e/1FAIpQLSfo-91_RaJoixJTWJJ5uA4JI0Ms7jkpO1YlhigUH6cDYQRI_Q/formResponse',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-142799032-1',
-  menu: [
-    {
-      label: 'About me',
-      path: '/pages/about',
+  menu: {
+    contact: {
+      label: 'Contact',
+      id: 'contact',
+      order: 1,
     },
-    {
-      label: 'Contact me',
-      path: '/contact',
+    technologies: {
+      label: 'Technologies',
+      id: 'technologies',
+      order: 2,
     },
-  ],
+    services: {
+      label: 'Services',
+      id: 'about',
+      order: 3,
+    },
+  },
   author: {
     name: 'QuarkOwl LTD',
     photo: '/logo.svg',
     contacts: {
       email: 'quarkowl@gmail.com',
-      twitter: 'burnpiro',
-      github: 'burnpiro',
+      twitter: 'quarkowl',
+      github: 'quarkowl',
       rss: '/rss.xml',
     },
   },
