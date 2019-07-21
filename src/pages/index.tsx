0b057508a39e9da829921b9f4b38025b1a8e5ff6
burnpiro <kemalpiro@gmail.com>
2019-07-21 10:40:25 +0200

    Create index for main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
 import * as React from 'react';
+import Layout from '../components/layout/Layout';
+import Hero from '../components/hero/Hero';
+import About from '../components/about/About';
+import TechnologyList from '../components/technology-list/TechnologyList';
+import ContactForm from '../components/contact-form/ContactForm';
+import { useSiteMetadata } from '../hooks';
 
-export default () => <div>Hello world!</div>;
+export default () => {
+  const { contactFormUrl, menu } = useSiteMetadata();
+
+  return (
+    <Layout>
+      <Hero />
+      <About id={menu.services.id} />
+      <TechnologyList id={menu.technologies.id} />
+      <ContactForm url={contactFormUrl} />
+    </Layout>
+  );
+};
