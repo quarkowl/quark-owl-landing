fb606fbe63302063864fac8c8e987b547b0741fa
burnpiro <kemalpiro@gmail.com>
2019-07-24 15:07:48 +0200

    Add Cookie banner

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-
 import TopBar from './TopBar';
 import Footer from './Footer';
-import { useSiteMetadata,useTAndCDialog } from '../../hooks';
+import { useSiteMetadata, useTAndCDialog } from '../../hooks';
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
@@ -13,6 +12,8 @@ import TAndC from '../t-and-c/TAndC';
 import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import SEO from '../seo/SEO';
+import CookieConsent from 'react-cookie-consent';
+import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles(theme => ({
   pageContainer: {
@@ -23,13 +24,13 @@ const useStyles = makeStyles(theme => ({
     maxWidth: '100vw',
   },
   placeHolder: {
-    flex: 1
-  }
+    flex: 1,
+  },
 }));
 
 export default function Layout({ children }) {
   const classes = useStyles(undefined);
-  const [ isDialogOpen, setIsDialogOpen ] = useTAndCDialog();
+  const [isDialogOpen, setIsDialogOpen] = useTAndCDialog();
   const { menu } = useSiteMetadata();
 
   const handleCloseDialog = () => {
@@ -48,11 +49,9 @@ export default function Layout({ children }) {
         aria-labelledby="privacy-policy-dialog"
         open={isDialogOpen}
       >
-        <DialogTitle id="privacy-policy-dialog">
-          Privacy Policy
-        </DialogTitle>
+        <DialogTitle id="privacy-policy-dialog">Privacy Policy</DialogTitle>
         <DialogContent dividers={true}>
-          <TAndC/>
+          <TAndC />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseDialog} color="primary">
@@ -60,6 +59,19 @@ export default function Layout({ children }) {
           </Button>
         </DialogActions>
       </Dialog>
+      <CookieConsent location="bottom">
+        This website uses cookies to enhance the user experience. Please read
+        our{' '}
+        <Link
+          component="a"
+          variant="h6"
+          underline="hover"
+          color="secondary"
+          onClick={() => setIsDialogOpen(true)}
+        >
+          Privacy Policy
+        </Link>
+      </CookieConsent>
     </React.Fragment>
   );
-};
+}
