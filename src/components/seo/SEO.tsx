c20b1f8c47837403f5239e750ad0c0e7a30a80de
burnpiro <kemalpiro@gmail.com>
2019-07-24 14:14:22 +0200

    Add html lang parameter

--- src/components/seo/SEO.tsx
+++ src/components/seo/SEO.tsx
@@ -17,29 +17,29 @@ const SEO: React.FC<Props> = ({ title }) => {
   const siteTitle = title || defaultTitle;
   const image = `${url}${author.photo}`;
   return (
-    <div>
-      s
-      <Helmet>
-        <title>{siteTitle}</title>
-        <meta name="description" content={description} />
-        <meta name="msapplication-TileColor" content={theme_color} />
-        <meta name="theme-color" content={theme_color} />
-        <meta name="image" content={image} />
-        <link rel="canonical" href={url} />
+    <Helmet
+      htmlAttributes={{
+        lang: 'en',
+      }}>
+      <title>{siteTitle}</title>
+      <meta name="description" content={description} />
+      <meta name="msapplication-TileColor" content={theme_color} />
+      <meta name="theme-color" content={theme_color} />
+      <meta name="image" content={image} />
+      <link rel="canonical" href={url} />
 
-        <meta property="og:url" content={url} />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content={author.contacts.twitter} />
-        <meta name="twitter:title" content={siteTitle} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-      </Helmet>
-    </div>
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={author.contacts.twitter} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+    </Helmet>
   );
 };
 
